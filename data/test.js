e9471bc0efa3fa196176833d9ed28b143d7d6305
Ye Wang <ywang@onshape.com>
2017-01-17 08:16:41 -0800

    read bvh and match the dae skin

---
+++ data/test.js
@@ -0,0 +1,685 @@
+var testdata = [{
+	"Displacement": {
+		"x": "2.7865",
+		"y": "3.9982",
+		"z": "5.7739"
+	},
+	"Rotation": {
+		"x": "87.59574",
+		"y": "5.030659",
+		"z": "-1.235185"
+	},
+	"name": "Hips"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "-11.54262",
+		"y": "-1.303108",
+		"z": "0.2360256"
+	},
+	"name": "RightUpLeg"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "-2.513474",
+		"y": "-57.37097",
+		"z": "1.089642"
+	},
+	"name": "RightLeg"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "0.01528221",
+		"y": "-47.97204",
+		"z": "0.4960888"
+	},
+	"name": "RightFoot"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "-29.69805",
+		"y": "50.07493",
+		"z": "-19.07222"
+	},
+	"name": "LeftUpLeg"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "0.04267224",
+		"y": "-47.9887",
+		"z": "-0.01273234"
+	},
+	"name": "LeftLeg"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "-0.1872824",
+		"y": "13.21085",
+		"z": "12.41571"
+	},
+	"name": "LeftFoot"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "11.53673",
+		"y": "-1.832593",
+		"z": "0.1245767"
+	},
+	"name": "RightShoulder"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "4.575474",
+		"y": "-13.42264",
+		"z": "-3.659722"
+	},
+	"name": "RightArm"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "0.217679",
+		"y": "-48.1507",
+		"z": "0.08481029"
+	},
+	"name": "RightForeArm"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "-6.000433",
+		"y": "67.99746",
+		"z": "-3.6831"
+	},
+	"name": "RightHand"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "-0.01524678",
+		"y": "-48.0864",
+		"z": "-0.1509458"
+	},
+	"name": "LeftShoulder"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "6.36059",
+		"y": "14.41257",
+		"z": "12.03308"
+	},
+	"name": "LeftArm"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "-0.02181204",
+		"y": "16.64909",
+		"z": "-0.1091037"
+	},
+	"name": "LeftForeArm"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "1.294458",
+		"y": "4.777986",
+		"z": "3.450042"
+	},
+	"name": "LeftHand"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "0.01964334",
+		"y": "11.35301",
+		"z": "0.01419446"
+	},
+	"name": "Head"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "0.3429972",
+		"y": "1.207656",
+		"z": "0.8523399"
+	},
+	"name": "Neck"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "0.06871533",
+		"y": "11.78076",
+		"z": "0.08069177"
+	},
+	"name": "Spine3"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "0.6240124",
+		"y": "2.005651",
+		"z": "1.422353"
+	},
+	"name": "Spine2"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "0.04828071",
+		"y": "11.30936",
+		"z": "0.02303548"
+	},
+	"name": "Spine1"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "-0.3154994",
+		"y": "0.3679746",
+		"z": "-0.1238014"
+	},
+	"name": "Spine"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "0.2076269",
+		"y": "9.058633",
+		"z": "0.4318627"
+	},
+	"name": "RightHandThumb1"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "-6.007022",
+		"y": "6.041378",
+		"z": "-2.540018"
+	},
+	"name": "RightHandThumb2"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "-3.463611",
+		"y": "8.063252",
+		"z": "-0.03760767"
+	},
+	"name": "RightHandThumb3"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "-13.61447",
+		"y": "4.158672",
+		"z": "-4.038377"
+	},
+	"name": "RightInHandIndex"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "-14.02185",
+		"y": "0.0158016",
+		"z": "-0.02861575"
+	},
+	"name": "RightHandIndex1"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "-13.05498",
+		"y": "3.829655",
+		"z": "45.10706"
+	},
+	"name": "RightHandIndex2"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "-28.97655",
+		"y": "0.07687795",
+		"z": "0.09028297"
+	},
+	"name": "RightHandIndex3"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "44.86517",
+		"y": "27.58252",
+		"z": "28.90886"
+	},
+	"name": "RightInHandMiddle"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "-27.96324",
+		"y": "0.02695714",
+		"z": "0.1211279"
+	},
+	"name": "RightHandMiddle1"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "4.400398",
+		"y": "16.70518",
+		"z": "-8.843961"
+	},
+	"name": "RightHandMiddle2"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "-2.7024",
+		"y": "0.2064",
+		"z": "3.388"
+	},
+	"name": "RightHandMiddle3"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "21.86",
+		"y": "0",
+		"z": "30"
+	},
+	"name": "RightInHandRing"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "-3.9984",
+		"y": "0",
+		"z": "0"
+	},
+	"name": "RightHandRing1"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "-10",
+		"y": "0",
+		"z": "0"
+	},
+	"name": "RightHandRing2"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "-2.7776",
+		"y": "0",
+		"z": "0"
+	},
+	"name": "RightHandRing3"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "-10",
+		"y": "0",
+		"z": "0"
+	},
+	"name": "RightInHandPinky"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "-3.5",
+		"y": "0.552",
+		"z": "2.148"
+	},
+	"name": "RightHandPinky1"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"name": "RightHandPinky2"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "-5.664",
+		"y": "-0.0992",
+		"z": "1.0848"
+	},
+	"name": "RightHandPinky3"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "2.55",
+		"y": "0",
+		"z": "20"
+	},
+	"name": "LeftHandThumb1"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "-3.9296",
+		"y": "0",
+		"z": "0"
+	},
+	"name": "LeftHandThumb2"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "0",
+		"y": "0",
+		"z": "24"
+	},
+	"name": "LeftHandThumb3"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "-2.228",
+		"y": "0",
+		"z": "0"
+	},
+	"name": "LeftInHandIndex"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "0",
+		"y": "0",
+		"z": "20"
+	},
+	"name": "LeftHandIndex1"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "-3.672",
+		"y": "0.5616",
+		"z": "0.8216"
+	},
+	"name": "LeftHandIndex2"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"name": "LeftHandIndex3"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "-5.617599",
+		"y": "-0.09119999",
+		"z": "0.3408"
+	},
+	"name": "LeftInHandMiddle"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "0",
+		"y": "0",
+		"z": "20"
+	},
+	"name": "LeftHandMiddle1"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "-4.288",
+		"y": "0",
+		"z": "0"
+	},
+	"name": "LeftHandMiddle2"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "0",
+		"y": "0",
+		"z": "24"
+	},
+	"name": "LeftHandMiddle3"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "-2.688",
+		"y": "0",
+		"z": "0"
+	},
+	"name": "LeftInHandRing"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "0",
+		"y": "0",
+		"z": "20"
+	},
+	"name": "LeftHandRing1"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "-3.6536",
+		"y": "0.5840001",
+		"z": "-0.14"
+	},
+	"name": "LeftHandRing2"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"name": "LeftHandRing3"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "-5.032",
+		"y": "-0.024",
+		"z": "-0.52"
+	},
+	"name": "LeftInHandPinky"
+}, {
+	"Displacement": {
+		"x": "0",
+		"y": "0",
+		"z": "0"
+	},
+	"Rotation": {
+		"x": "-3",
+		"y": "0",
+		"z": "20"
+	},
+	"name": "LeftHandPinky1"
+}]
